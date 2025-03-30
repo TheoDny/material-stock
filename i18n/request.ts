af6db3d1e2b6e9ff7b3e13bcbfa1c86f8d73e7a0
Theo Denoyelle <denoyelletheo@gmail.com>
2025-03-30 21:10:30 +0200

    feat: i18n use "accept-language" header by default

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,5 +1,5 @@
-import { getRequestConfig } from "next-intl/server"
-import { cookies } from "next/headers"
+import { getRequestConfig, getLocale } from "next-intl/server"
+import { cookies, headers } from "next/headers"
 
 export const IMPLEMENTED_LOCALE = ["en", "fr"]
 
@@ -7,7 +7,13 @@ export default getRequestConfig(async () => {
     let locale = (await cookies()).get("NEXT_LOCALE")?.value || "en"
 
     if (!IMPLEMENTED_LOCALE.includes(locale)) {
-        locale = "en"
+        const h = (await headers()).get("accept-language")
+        let l
+        if (h && h.length > 2) {
+            l = h[0] + h[1]
+        }
+
+        locale = l || "en"
     }
 
     return {
